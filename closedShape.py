40e42bae73711955fd6daa7a2042bcbabd2e0704
MorganeFar <131769763+MorganeFar@users.noreply.github.com>
2025-02-17 22:13:26 +0100

    feat : test black border

--- closedShape.py
+++ closedShape.py
@@ -8,7 +8,8 @@ class ClosedShape(inkex.EffectExtension):
     def effect(self):
         for element in self.document.getroot().iter():
             element_id = element.get_id()
-            if re.search("^path.*$", element_id):
+            # check if the element is a path and if its border is black
+            if re.search("^path.*$", element_id) and (element.style.get_color(name='stroke').red == 0 and element.style.get_color(name='stroke').blue == 0 and element.style.get_color(name='stroke').red == 0):
                 path = element.get_path()
 
                 if re.search("^.*[zZ]$", str(path)): # closed path
