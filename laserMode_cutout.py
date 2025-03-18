00a8bd6d15da4f42fd51f4c35345ef8a099ec3a0
LilianCUSUMANO <liliancusumano@gmail.com>
2025-03-18 09:05:51 +0100

    eng names

---
+++ laserMode_cutout.py
@@ -0,0 +1,21 @@
+import inkex
+from inkex import EffectExtension, ShapeElement
+
+class AfficherElementsSelectionnes(EffectExtension):
+    def effect(self):
+        selection = self.svg.selection
+
+        if not selection:
+            inkex.errormsg("Aucun élément sélectionné.")
+            return
+
+        for element in selection:
+            if isinstance(element, ShapeElement):
+                element.style['fill'] = 'none'
+
+                element.style['stroke'] = '#ff0000'  
+                element.style['stroke-width'] = '1px'  
+
+              
+if __name__ == '__main__':
+    AfficherElementsSelectionnes().run()
